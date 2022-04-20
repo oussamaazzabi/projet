e1a3de5b70ddf95363df3467a5b50a14a1e25f0f
oussama@DESKTOP-HB7D939.(none) <oussama.azzabi001@gmail.com>
2022-04-20 23:34:58 +0100

    oussama

---
+++ app/src/main/java/com/example/sem/User.java
@@ -0,0 +1,18 @@
+package com.example.sem;
+
+public class User {
+
+    public String fullName, age, email,password;
+
+    public User(){
+
+    }
+
+    public User(String fullName,String age, String email,String password){
+        this.fullName= fullName;
+        this.age= age;
+        this.email= email;
+        this.password=password;
+    }
+
+}
